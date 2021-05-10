d2e3bcff272358f4e51254ff635c655990345600
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-05-10 22:43:11 +0100

    Pass through string ID straight from sequencer to tracks

--- sequencer.py
+++ sequencer.py
@@ -3,28 +3,17 @@ from .track import Track
 
 
 class Sequencer:
-    TRACKS = {
-        "kick": Track(Track.KICK, metronome),
-        "snare": Track(Track.SNARE, metronome),
-        "perc": Track(Track.PERC, metronome),
-        "sample": Track(Track.SAMPLE, metronome),
-        "bass": Track(Track.BASS, metronome),
-        "lead": Track(Track.LEAD, metronome),
-        "arp": Track(Track.ARP, metronome),
-        "chord": Track(Track.CHORD, metronome),
-    }
-
     def __init__(self, bpm=120, ticks_per_beat=4):
         Metronome().bpm = bpm
         Metronome().ticks_per_beat = ticks_per_beat
-        self.kick = Sequencer.TRACKS[kick]
-        self.snare = Sequencer.TRACKS[snare]
-        self.perc = Sequencer.TRACKS[perc]
-        self.sample = Sequencer.TRACKS[sample]
-        self.bass = Sequencer.TRACKS[bass]
-        self.lead = Sequencer.TRACKS[lead]
-        self.arp = Sequencer.TRACKS[arp]
-        self.chord = Sequencer.TRACKS[chord]
+        self.kick = Track("kick")
+        self.snare = Track("snare")
+        self.perc = Track("perc")
+        self.sample = Track("sample")
+        self.bass = Track("bass")
+        self.lead = Track("lead")
+        self.arp = Track("arp")
+        self.chord = Track("chord")
 
     def start(self):
         Metronome().tick_forever()
